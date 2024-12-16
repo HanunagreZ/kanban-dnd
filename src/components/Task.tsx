ae1d892e4d2ec4607e942ea1821229094a4f8866
HanunagreZ <hanunagrez@yandex.ru>
2024-12-16 11:54:33 +0300

    feat: add animation to cart (create / delete)

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,7 +1,7 @@
-import { Draggable } from 'react-beautiful-dnd';
 import { Id, TTask } from '../types';
 import { useState } from 'react';
 import TrashIcon from '../icons/TrashIcon';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface TaskProps {
   task: TTask;
@@ -9,14 +9,30 @@ interface TaskProps {
   updateTask: (id: Id, title: string) => void;
 }
 
-const Task = (props: TaskProps) => {
-  const { task, deleteTask, updateTask } = props;
 
+const Task = (props: TaskProps) => {
+
+  const { task, deleteTask, updateTask } = props;
   const [mouseIsOver, setMouseIsOver] = useState(false);
   const [editing, setEditing] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  const handleDelete = () => {
+    setIsDeleted(true);
+    setTimeout(() => deleteTask(task.id), 300);
+  };
 
   return (
-    <div
+
+    <AnimatePresence>
+      {!isDeleted && (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.5 }}
+          transition={{ duration: 0.3 }}
+        >
+          <div
       onMouseEnter={() => setMouseIsOver(true)}
       onMouseLeave={() => setMouseIsOver(false)}
       onClick={() => setEditing(true)}
@@ -41,14 +57,29 @@ const Task = (props: TaskProps) => {
       {mouseIsOver && (
         <button
           className="hover:stroke-white stroke-gray-500 px-1 py-2 hover:bg-columnBackgroundColor"
-          onClick={() => {
-            deleteTask(task.id);
-          }}
+          onClick={handleDelete}
         >
           <TrashIcon />
         </button>
       )}
     </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
   );
 };
 
